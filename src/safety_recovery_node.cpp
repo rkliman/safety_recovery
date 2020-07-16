58bd9e931a491b6177058eeaa449f6fe4f5ffd5b
Randall Kliman <rkliman11@gmail.com>
2020-07-16 15:32:39 -0500

    increased robustness, asyncMove won't stop though

--- src/safety_recovery_node.cpp
+++ src/safety_recovery_node.cpp
@@ -3,6 +3,7 @@
 **  @author Randall Kliman
 **/
 #include <ros/ros.h>
+
 #include <string>
 #include <ur_dashboard_msgs/SafetyMode.h>
 #include <ur_robot_driver/ros/dashboard_client_ros.h>
@@ -15,42 +16,41 @@ int main(int argc, char* argv[])
   ros::AsyncSpinner async_spinner(1);
   async_spinner.start();
   ros::NodeHandle nh;
+  bool isMoving = false;
 
-  //ur_driver::DashboardClientROS dashboard_client(nh, "192.168.100.10");
-  //std_srvs::Trigger dash_client;
   ros::ServiceClient safety_client = nh.serviceClient<ur_dashboard_msgs::GetSafetyMode>("/ur_hardware_interface/dashboard/get_safety_mode");
   ros::ServiceClient state_client = nh.serviceClient<ur_dashboard_msgs::IsProgramRunning>("/ur_hardware_interface/dashboard/program_running");
   ros::ServiceClient unlock_client = nh.serviceClient<std_srvs::Trigger>("/ur_hardware_interface/dashboard/unlock_protective_stop");
+  ros::ServiceClient brake_client = nh.serviceClient<std_srvs::Trigger>("/ur_hardware_interface/dashboard/brake_release");
   ros::ServiceClient play_client = nh.serviceClient<std_srvs::Trigger>("/ur_hardware_interface/dashboard/play");
   ros::ServiceClient power_on_client = nh.serviceClient<std_srvs::Trigger>("/ur_hardware_interface/dashboard/power_on");
+  ros::ServiceClient load_client = nh.serviceClient<std_srvs::Trigger>("/ur_hardware_interface/dashboard/load_program");
   ur_dashboard_msgs::GetSafetyMode get_mode;
   ur_dashboard_msgs::IsProgramRunning prog_state;
+  ur_dashboard_msgs::Load load;
+  load.request.filename = "excontrol.urp";
   std_srvs::Trigger unlock;
+  std_srvs::Trigger release;
   std_srvs::Trigger play;
   std_srvs::Trigger power_on;
 
+  //call a startup sequence
+  ROS_INFO("Powering On");
   power_on_client.call(power_on);
+  ROS_INFO("Unlocking");
+  brake_client.call(release);
+  ROS_INFO("Loading Program");
+  load_client.call(load);
+  ROS_INFO("Playing Program");
   play_client.call(play);
 
 
   //make sure to run ur10_e_moveit_planning_execution.launch before running this node. Otherwise "manipulator" will not be found
   moveit::planning_interface::MoveGroupInterface move_group("manipulator");
   move_group.setPoseReferenceFrame("base_link");
-  // geometry_msgs::Point target_p;
-  // target_p.x = 1;
-  // target_p.y = 0;
-  // target_p.z = 0;
-  // geometry_msgs::Quaternion target_o;
-  // target_o.x = 0;
-  // target_o.y = 0;
-  // target_o.z = 0;
-  // target_o.w = 0;
-  // geometry_msgs::Pose move_target;
-  // move_target.position = target_p;
-  // move_target.orientation = target_o;
-  // move_group.setPoseTarget(move_target);
   move_group.setNamedTarget("home");
   move_group.asyncMove();
+  isMoving = true;
 
 
   while(ros::ok()) {
@@ -70,9 +70,24 @@ int main(int argc, char* argv[])
     //     ros::Duration(6).sleep();
     //     unlock_client.call(unlock);
     // }
+
     if (get_mode.response.answer.find("NORMAL") == std::string::npos) {
         move_group.stop();
+        move_group.stop();
+        isMoving = false;
+        ROS_INFO("Stopping");
+        ros::Duration(6).sleep();
+        ROS_INFO("Unlocking");
+        unlock_client.call(unlock);
         ros::Duration(3).sleep();
+        play_client.call(play);
+        ROS_INFO("Playing");
+        // ros::Duration(3).sleep();
+        do
+        {
+          ROS_WARN("Protective Stop detected. Please move the robot to a safe position");
+          std::cout << '\n' << "Press a key to continue...";
+        } while (std::cin.get() != '\n');
     }
 
     //Restarts program once a protective stop is cleared
@@ -81,9 +96,12 @@ int main(int argc, char* argv[])
         ROS_INFO("Program Stopped, Running Program...");
         ros::Duration(2).sleep();
         play_client.call(play);
-        move_group.setNamedTarget("home");
-        move_group.asyncMove();
-        //goToPose();
+    }
+
+    if (isMoving == false) {
+      move_group.setNamedTarget("home");
+      move_group.asyncMove();
+      isMoving = true;
     }
     ros::Duration(0.5).sleep();
   }
